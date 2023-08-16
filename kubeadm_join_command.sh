efb85f40b3a55232017bc60073b59522ffb60727
EC2 Default User <ec2-user@ip-10-0-1-250.ec2.internal>
2023-08-16 18:16:35 +0000

    kube-join file added

--- kubeadm_join_command.sh
+++ kubeadm_join_command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo kubeadm join 10.0.1.169:6443 --token yddhv7.kgmgsl2lyi4iyxoj --discovery-token-ca-cert-hash sha256:cbe6bae0ef1c9b5026f21b3b9114ecf7bfaa6446ca03374481340e47d33d0cfa 
+sudo kubeadm join 10.0.1.250:6443 --token vxi5sm.r1l3vbbj4mr1c641 --discovery-token-ca-cert-hash sha256:c02009c633a3b2637269daaaeba52ecddd75f8bdd196e9d53e26b86c05b59fde 
